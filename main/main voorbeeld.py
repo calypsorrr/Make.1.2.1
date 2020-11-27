de1b0f0394e7c7bc982a6b461955ae14ade98706
calypsorrr <74073146+calypsorrr@users.noreply.github.com>
2020-11-27 13:02:45 +0100

    Add files via upload

---
+++ main/main voorbeeld.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Info about our project comes here
+"""
+
+import myscripy
+
+__author__ = "Bo Claes"
+__email__ = "bo.claes@student.kdg.be"
+__status__ = "Development"
+
+
+if __name__ == "__main__":
+    my_function()
+
+    myscripy.my_function()
